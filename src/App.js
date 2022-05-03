4bdaad5d895920e83e0de3f6a5dba799b99fd43f
Katzablanka <72996614+Katzablanka@users.noreply.github.com>
2022-05-03 15:06:47 +0200

    funk/class comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ClassCounter></ClassCounter>
+      <HookCounter></HookCounter>
+    </div>
+  );
 }
 
 export default App;
